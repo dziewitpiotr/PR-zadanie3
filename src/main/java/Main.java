3059c83c964f0159b9d911089830d7b484c2bc95
Piotr Dziewit <dziewitpiotr@gmail.com>
2020-02-22 11:46:09 +0100

    zadanie3 zrobione

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) throws MixedWordException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String tokeny = scanner.next();
+
+        metoda(tokeny);
+
+//        //drugie podejscie
+//        for (int i = 0; i < tokeny.length(); i++) {
+//        }
+
+    }
+
+    private static void metoda(String token) throws MixedWordException {
+        for (int i = 0; i < token.length()-1; i++) {
+
+            if(((token.charAt(i) >= 48 && token.charAt(i) <= 57) && (token.charAt(i+1) >= 48 && token.charAt(i+1) <= 57)) || ((token.charAt(i) >= 97 && token.charAt(i) <= 122) && (token.charAt(i+1) >= 97 && token.charAt(i+1) <= 122))) {
+                return;
+            }
+
+        }
+        throw new MixedWordException();
+    }
+
 }
---
+++ src/main/java/MixedWordException.java
@@ -0,0 +1,3 @@
+public class MixedWordException extends Throwable {
+
+}
